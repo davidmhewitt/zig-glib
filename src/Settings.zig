c68af9ed1bd07cfe189e9d142ee0a350fd6d9bd6
David Hewitt <davidmhewitt@gmail.com>
2022-03-24 20:25:10 +0000

    Add changed signal to GSettings

--- src/Settings.zig
+++ src/Settings.zig
@@ -1,3 +1,5 @@
+const c = @import("c.zig");
+
 pub const Settings = extern struct {
     const Self = @This();
 
@@ -12,6 +14,10 @@ pub const Settings = extern struct {
     extern fn g_settings_get_string(self: *Self, key: [*c]const u8) [*c]const u8;
     /// Caller owns returned memory, must be freed with GLib.free
     pub const getString = g_settings_get_string;
+
+    pub fn connectChanged(self: *Self, callback: fn (key: [*c]const u8, data: ?*anyopaque) callconv(.C) void, data: ?*anyopaque) u64 {
+        return c.g_signal_connect_data(self, "changed", @ptrCast(c.GCallback, callback), data, null, @as(c.GConnectFlags, 0));
+    }
 
     extern fn g_object_unref(self: *Self) void;
     pub const unref = g_object_unref;
